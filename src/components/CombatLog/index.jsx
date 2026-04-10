069c26afaa14b260fd85d175324e917350ac5b17
Bran van der Meer <bran.van.der.meer@protonmail.com>
2026-04-10 15:07:50 +0200

    feat: Improve autoscroll in CombatLog, add toggle button

--- src/components/CombatLog/index.jsx
+++ src/components/CombatLog/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react'
+import { useState, useRef, useEffect, useCallback } from 'react'
 import { SPELL_DATA } from '../../reducers/spell/data.js'
 import './index.css'
 
@@ -16,24 +16,49 @@ export default function CombatLog({ castHistory, targets }) {
   }
 
   const scrollRef = useRef()
+  const [autoScroll, setAutoScroll] = useState(true)
+
+  const checkAtBottom = useCallback(() => {
+    const el = scrollRef.current
+    if (!el) return
+    const atBottom = el.scrollTop + el.clientHeight >= el.scrollHeight - 4
+    setAutoScroll(atBottom)
+  }, [])
+
   useEffect(() => {
-    const lastElement = scrollRef.current.querySelector('li:last-child')
-    if (lastElement === null) return
-    lastElement.scrollIntoView(false)
-  }, [historyLength])
+    if (!autoScroll) return
+    scrollRef.current.scrollTop = scrollRef.current.scrollHeight
+  }, [historyLength, autoScroll])
+
+  const scrollToBottom = () => {
+    scrollRef.current.scrollTop = scrollRef.current.scrollHeight
+  }
 
   return (
-    <div className='CombatLog' ref={scrollRef}>
-      <ol className='CombatLog__list'>
-        {filteredCastHistory.map((cast) => {
-          const key = `${cast.timestamp}-${cast.type}-${cast.spellId}-${cast.targetId}`
-          return (
-            <li key={key} className='CombatLog__listitem'>
-              {cast.stringify}
-            </li>
-          )
-        })}
-      </ol>
+    <div className='CombatLog'>
+      <div
+        className='CombatLog__scroll'
+        ref={scrollRef}
+        onScroll={checkAtBottom}
+      >
+        <ol className='CombatLog__list'>
+          {filteredCastHistory.map((cast) => {
+            const key = `${cast.timestamp}-${cast.type}-${cast.spellId}-${cast.targetId}`
+            return (
+              <li key={key} className='CombatLog__listitem'>
+                {cast.stringify}
+              </li>
+            )
+          })}
+        </ol>
+      </div>
+      <button
+        className={`CombatLog__scrollBtn ${autoScroll ? 'CombatLog__scrollBtn--active' : ''}`}
+        onClick={scrollToBottom}
+        title={autoScroll ? 'Auto-scrolling' : 'Click to resume auto-scroll'}
+      >
+        ▼
+      </button>
     </div>
   )
 }
